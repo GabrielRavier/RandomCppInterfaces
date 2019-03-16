f4343fa2e08408f554d0b2a523033e85d45c46d6
Gabriel Ravier <gabravier@gmail.com>
2019-03-16 19:31:58 +0100

    Small amount of stuff:

--- mbState.hpp
+++ mbState.hpp
@@ -1,17 +1,54 @@
 #pragma once
 #include <cwchar>
+#include <cstring>
 
 namespace ravier
 {
 
 struct mbState
 {
-    mbstate_t state = { 0 };    // 0 is the initial conversion state
+    mbstate_t state;
+
+    mbState()
+    {
+        memset(&state, 0, sizeof(state));    // Initialize mbstate_t properly
+    }
+
+    void setInitial()
+    {
+        if (!this->representsInitialShiftState())
+            state = { 0 };
+    }
 
     bool representsInitialShiftState()
     {
         return mbsinit(&state) != 0;
     }
-}
+
+    size_t nextCharacterSize(const char *str, size_t maxBytes)
+    {
+        return mbrlen(str, maxBytes, &state);
+    }
+
+    size_t multiCharToWideChar(wchar_t *destination, const char *source, size_t limit)
+    {
+        return mbrtowc(destination, source, limit, &state);
+    }
+
+    size_t wideCharToMultiChar(char *destination, wchar_t charToConvert)
+    {
+        return wcrtomb(destination, charToConvert, &state);
+    }
+
+    size_t multiCharCStringToWideCharCString(wchar_t *destination, const char **source, size_t limit)
+    {
+        return mbsrtowcs(destination, source, limit, &state);
+    }
+
+    size_t wideCharCStringToMultiCharCString(char *destination, const wchar_t **source, size_t limit)
+    {
+        return wcsrtombs(destination, source, limit, &state);
+    }
+};
 
 }
